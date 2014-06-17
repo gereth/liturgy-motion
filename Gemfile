92731db5a81bdfaf1febb39a9389fa0369d7c193
Geoff Ereth <geoff+github@little.fm>
2014-06-17 13:51:26 -0700

    Add loop for audioController

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem "motion-cocoapods"
 gem "cocoapods"
-
+gem 'sugarcube', :require => 'sugarcube-classic'
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,9 +1,17 @@
+# http://hboon.com/rubymotion-tutorial-for-objective-c-developers/
+
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @audioController =
       AEAudioController.alloc.initWithAudioDescription(
         AEAudioController.nonInterleaved16BitStereoAudioDescription
       )
+    @audioController.start Pointer.new(:object)
+    audio = "audio/loop.m4a".resource_url
+    @track =  AEAudioFilePlayer.audioFilePlayerWithURL(audio, audioController: @audioController, error:nil)
+    @track.loop
+    channel = AEBlockChannel.channelWithBlock -> {}
+                                                    
     
     true
   end
