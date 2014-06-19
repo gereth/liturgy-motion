c240afbf84b1e70cb0e2b52032efc9b4e9f2dc29
Geoff Ereth <geoff+github@little.fm>
2014-06-19 10:33:55 -0700

    Add component desc with AudioUnit

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,27 +5,34 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
   
 
-    @audioController = WeakRef.new(
-      AEAudioController.alloc.initWithAudioDescription(
-        AEAudioController.nonInterleaved16BitStereoAudioDescription
-      )
-    )
-    @audioController.preferredBufferDuration = 0.005
+    @audioController= AEAudioController.alloc.initWithAudioDescription(AEAudioController.nonInterleaved16BitStereoAudioDescription)
+    # @audioController.preferredBufferDuration = 0.005
     err = Pointer.new(:object)
     @audioController.start err
     
     file = "audio/loop.m4a".resource_url
     @audio =  AEAudioFilePlayer.audioFilePlayerWithURL(file, audioController: @audioController, error:nil)
+    @audio.channelIsMuted = false
     @audio.loop = true
     
-    # component_desc = AEAudioComponentDescriptionMake(
-    #     KAudioUnitManufacturer_Apple, 
-    #     KAudioUnitType_Generator, 
-    #     KAudioUnitSubType_AudioFilePlayer
-    # )
-    # oUnitChannel.alloc.initWithComponentDescription(component_desc, audioController: @audiocontroller, error: nil)
-    # @window.makeKeyAndVisible
+    @audio2 =  AEAudioFilePlayer.audioFilePlayerWithURL(file, audioController: @audioController, error:nil)
+    @audio2.channelIsMuted = false
+    @audio2.loop = true
+
+    
+    component_desc = AEAudioComponentDescriptionMake(KAudioUnitManufacturer_Apple, KAudioUnitType_Generator, KAudioUnitSubType_AudioFilePlayer)
+    audio_unit_player = AEAudioUnitChannel.alloc.initWithComponentDescription(component_desc, audioController: @audiocontroller, error: nil)
+    @audioController.addChannels [@audio]
+    @audioController.addChannels audio_unit_player
+    
+    @window.rootViewController = UIViewController.new
+    
+    @window.makeKeyAndVisible
     true
+    # 
+    # sleep 10
+    # @audioController.addChannels [@audio2]
+    
   end
 end
 
@@ -43,4 +50,7 @@ end
 # AudioServicesPlaySystemSound(sound_id[0])
 # KPanningMode_SoundField
 
+# - not support for AudioUnit and iOS
+# - http://stackoverflow.com/questions/22960236/rubymotion-throwing-nameerror-when-trying-to-create-a-audiocomponentdescription
+# - http://stackoverflow.com/questions/14120878/ld-framework-not-found-audiounit
 
