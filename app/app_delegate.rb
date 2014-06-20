2e981e483589cc1bcb035e4a5a776cf1e1e440a3
Geoff Ereth <geoff+github@little.fm>
2014-06-20 11:06:49 -0700

    Adding limiter

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,47 +1,75 @@
 # http://hboon.com/rubymotion-tutorial-for-objective-c-developers/
 
 class AppDelegate
+
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-  
 
-    @audioController= AEAudioController.alloc.initWithAudioDescription(AEAudioController.nonInterleaved16BitStereoAudioDescription)
-    # @audioController.preferredBufferDuration = 0.005
-    err = Pointer.new(:object)
-    @audioController.start err
+    # Audio Controller
+    @audio_controller= AEAudioController.alloc.initWithAudioDescription(
+      AEAudioController.nonInterleaved16BitStereoAudioDescription
+    )
+    @audio_controller.start au_controller_error
     
-    file = "audio/loop.m4a".resource_url
-    @audio =  AEAudioFilePlayer.audioFilePlayerWithURL(file, audioController: @audioController, error:nil)
-    @audio.channelIsMuted = false
-    @audio.loop = true
+    # Player & Channels
+    audio_component = AEAudioComponentDescriptionMake(
+      KAudioUnitManufacturer_Apple, 
+      KAudioUnitType_Generator, 
+      KAudioUnitSubType_AudioFilePlayer
+    )
+    audio_unit_player = AEAudioUnitChannel.alloc.initWithComponentDescription(
+      audio_component, 
+      audioController: @audiocontroller, 
+      error: au_player_error
+    )
+    @audio = load_audio "audio/loop.m4a"
+    @audio_controller.addChannels [@audio]
+    @audio_controller.addChannels audio_unit_player
     
-    @audio2 =  AEAudioFilePlayer.audioFilePlayerWithURL(file, audioController: @audioController, error:nil)
-    @audio2.channelIsMuted = false
-    @audio2.loop = true
-
+    # Filter
+    delay_component = AEAudioComponentDescriptionMake(
+      KAudioUnitManufacturer_Apple, 
+      KAudioUnitType_Effect,
+      KAudioUnitSubType_Delay
+    )
+    @ae_filter = AEAudioUnitFilter.alloc.initWithComponentDescription(delay_component, audioController:@audio_controller, error: au_filter_error)
     
-    component_desc = AEAudioComponentDescriptionMake(KAudioUnitManufacturer_Apple, KAudioUnitType_Generator, KAudioUnitSubType_AudioFilePlayer)
-    audio_unit_player = AEAudioUnitChannel.alloc.initWithComponentDescription(component_desc, audioController: @audiocontroller, error: nil)
-    @audioController.addChannels [@audio]
-    @audioController.addChannels audio_unit_player
+    @limiter = AELimiterFilter.alloc.initWithAudioController(@audio_controller)
+    @limiter.level = 100.0
     
     @window.rootViewController = UIViewController.new
-    
     @window.makeKeyAndVisible
     true
-    # 
-    # sleep 10
-    # @audioController.addChannels [@audio2]
-    
   end
+  
+  protected
+  
+  def load_audio(path, opts={})
+    audio = AEAudioFilePlayer.audioFilePlayerWithURL(path.resource_url, audioController: @audio_controller, error:nil)
+    audio.channelIsMuted = false
+    audio.loop = opts[:loop] || false
+    audio
+  end
+  
+  %w( au_controller au_player au_filter).each do |name|
+    define_method("#{name}_error") do
+      Pointer.new(:object)
+    end
+  end
+  
+  def auto_pan(channel, origin, dest, speed=0.033)
+    (origin.to_f..dest).step(0.01).to_a.each do |pan|
+      puts "panning: #{pan}"
+      channel.pan = pan
+    end
+  end
+  
+  def instance(name)
+    App.delegate.instance_variable_get(:"@#{name}")
+  end
+    
 end
 
-
-#- REPL debugging
-# delegate = UIApplication.sharedApplication.delegate
-# delegate.instance_variable_get(:@audioController)
-
-#- Issue with Motion and AudioUnit bridgesupport
 # https://groups.google.com/forum/#!topic/rubymotion/-IBIElWiQFI
 
 # - AudioToolbox
@@ -53,4 +81,8 @@ end
 # - not support for AudioUnit and iOS
 # - http://stackoverflow.com/questions/22960236/rubymotion-throwing-nameerror-when-trying-to-create-a-audiocomponentdescription
 # - http://stackoverflow.com/questions/14120878/ld-framework-not-found-audiounit
+
+# - Compression
+# http://club15cc.com/code/ios/mixing-audio-without-clipping-limiters-etc
+# https://developer.apple.com/library/ios/documentation/AudioUnit/Reference/AudioUnitParametersReference/Reference/reference.html#//apple_ref/doc/uid/TP40007290-CH1-SW1
 
