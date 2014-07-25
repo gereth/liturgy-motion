7e8603d979c3185f20d258d9a0e3e03957a74d87
Geoff Ereth <geoff+github@little.fm>
2014-07-24 18:33:12 -0700

    Don't need the navbar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,12 +6,12 @@ class AppDelegate
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-    nav_bar = UINavigationBar.appearance
-    nav_bar.setTitleTextAttributes({
-      UITextAttributeFont            => UIFont.fontWithName('AvenirNext-Bold', size: 16),
-      UITextAttributeTextShadowColor => :clear.uicolor,
-      UITextAttributeTextColor       => :white.uicolor
-    })
+    # nav_bar = UINavigationBar.appearance
+    # nav_bar.setTitleTextAttributes({
+    #   UITextAttributeFont            => UIFont.fontWithName('AvenirNext-Bold', size: 16),
+    #   UITextAttributeTextShadowColor => :clear.uicolor,
+    #   UITextAttributeTextColor       => :white.uicolor
+    # })
 
     root_controller = RootController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller  = UINavigationController.alloc.initWithRootViewController(root_controller)
