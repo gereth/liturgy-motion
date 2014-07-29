55b50cc51253fe61b60ca5c9828d5826f760761a
Geoff Ereth <geoff+github@little.fm>
2014-07-29 11:08:43 -0700

    Use UIScrollView for animations

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,7 +2,19 @@ class RootController < UIViewController
 
   def viewDidLoad
     super
+    setup_scroll_view
     setup_locations
+    self.view.addSubview @scroll_view
+  end
+
+  def setup_scroll_view
+    @scroll_view = UIScrollView.alloc.init
+    @scroll_view.frame = CGRectMake(0, 0, App.window.frame.size.width, App.window.frame.size.height)
+    @scroll_view.pagingEnabled = false
+    @scroll_view.backgroundColor = UIColor.blackColor
+    @scroll_view.contentSize = CGSizeMake(@scroll_view.frame.size.width, @scroll_view.frame.size.height)
+    @scroll_view.showsVerticalScrollIndicator = false
+    @scroll_view.delegate = self
   end
 
   def locations
@@ -11,18 +23,18 @@ class RootController < UIViewController
 
   def setup_locations
     locations.each_with_index do |location, idx|
-      self.view << location_gif_btn(location, idx)
+      @scroll_view << location_gif_btn(location, idx)
     end
   end
 
-    def location_animation(location)
-      images = gif_images(location).compact
-      view =  UIImageView.alloc.initWithFrame(CGRectMake(0, 0, 320, 284))
-      view.animationImages = images
-      view.animationDuration = 5.0
-      view.startAnimating
-      view
-    end
+  def location_animation(location)
+    images = gif_images(location).compact
+    view =  UIImageView.alloc.initWithFrame(CGRectMake(0, 0, 320, 284))
+    view.animationImages = images
+    view.animationDuration = 60.0
+    view.startAnimating
+    view
+  end
 
   def gif_images(location)
     Dir.glob(File.join("buttons/locations".resource_path, location, "*")).map do |path|
@@ -31,9 +43,8 @@ class RootController < UIViewController
   end
 
   def location_gif_btn(location, index)
-    y_axis = index == 0 ? 0 : 284
-
     btn = UIButton.buttonWithType(:custom.uibuttontype)
+    y_axis = index == 0 ? 0 : 284
     btn.setFrame(CGRectMake(0, y_axis, 320, 284))
     btn.tag = index
     btn.addTarget(self, action:"location_action:", forControlEvents: :touch.uicontrolevent)
@@ -44,18 +55,6 @@ class RootController < UIViewController
     btn.alpha = 0
     btn.fade_in(duration: 0.9, delay: index.to_f)
   end
-
-  # def location_btn(location, index)
-  #   btn    = UIButton.buttonWithType(:custom.uibuttontype)
-  #   img    = location_img(location)
-  #   y_axis = index == 0 ? 0 : img.size.height
-  #   btn.setFrame(CGRectMake(0, y_axis, img.size.width, img.size.height))
-  #   btn.tag = index
-  #   btn.addTarget(self, action:"location_action:", forControlEvents: :touch.uicontrolevent)
-  #   btn.setBackgroundImage(img, forState:UIControlStateNormal)
-  #   btn.alpha = 0
-  #   btn.fade_in(duration: 0.8, delay: "#{index}".to_f)
-  # end
 
   def location_img(location)
     "buttons/locations/#{location}_#{screen_height}".uiimage
