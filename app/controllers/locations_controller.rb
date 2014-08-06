0c3e808c01b70b38527000522581348c48264f22
Geoff Ereth <geoff+github@little.fm>
2014-08-06 14:44:35 -0700

    Add get_directions for location navbar

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,7 @@ class LocationsController < UIViewController
     puts "<> Initializing #{location}"
     @location = location
     @arranger = Arranger.new(location)
+     # get_directions
   end
 
   def viewDidLoad
@@ -13,7 +14,20 @@ class LocationsController < UIViewController
     arranger.start if within_distance_of(location)
   end
 
+  # http://stackoverflow.com/questions/14028033/need-assistance-regarding-mkmapitem
   def within_distance_of(location)
+    #
+    #  Use accessors setup in delegate for current position ...
+    #  Arrangement should incude 'noise' if more than X meter from Location
+  end
+
+  def get_directions
+    location_item = LocationItem.new(location)
+    placemark = MKPlacemark.alloc.initWithCoordinate(location_item.coordinate, addressDictionary:location_item.address )
+    map_item  = MKMapItem.alloc.initWithPlacemark(placemark)
+    map_item.name = location_item.name
+    opts = {}
+    map_item.openInMapsWithLaunchOptions(opts)
   end
 
   def viewWillDisappear(animated)
